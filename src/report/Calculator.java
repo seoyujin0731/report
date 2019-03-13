3411bbf44f03725eb75e46878d45b6e9563e63f3
tjdbwls0731 <tjdbwls8998@naver.com>
2019-03-13 14:20:51 +0900

    report

---
+++ src/report/Calculator.java
@@ -0,0 +1,40 @@
+
+package report;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		String x = sc.next();
+		double b = sc.nextDouble();
+
+		switch (x) {
+		
+		case "+":
+			System.out.println(a + "+" + b + "=" + (a + b));
+			break;
+
+		case "-":
+			System.out.println(a + "-" + b + "=" + (a - b));
+			break;
+
+		case "*":
+			System.out.println(a + "*" + b + "=" + (a * b));
+			break;
+
+		case "/":
+			if (b == 0) {
+				System.out.println("0으로 나눌 수 없습니다.");
+			} else {
+				System.out.println(a + "/" + b + "=" + (a / b));
+			}
+			break;
+
+		}
+	}
+
+}
